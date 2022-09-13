20855d6539c0fc368817cf002a6fb8c2bbe6038d
Md. Alamin Hossain <96656016+alamin486@users.noreply.github.com>
2022-09-13 17:55:35 +0600

    Add files via upload

---
+++ create variable and print output/javascript.js
@@ -0,0 +1,7 @@
+//create 2 variable
+let firstName= "Alamin"
+let lastName= "Hossain"
+//concatenate the two variable into third variable
+
+let fullName= firstName+" "+lastName
+console.log(fullName)
